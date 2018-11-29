345663d5c5c3050a42c5ee9dc809b8bab31a3b77
otherness-space <the@otherness.space>
2018-11-29 14:36:35 -0500

    edit changes

--- fizz-buzz/script.js
+++ fizz-buzz/script.js
@@ -1,15 +1,29 @@
-let iterate = 1; // intialization
+let num = 1;
+while (num <= 100) { // num <101
 
-while(iterate < 101) // condition
-  {
-    if( (iterate / 5 % 2 === (0 || 1) ) && (iterate / 3 % 2 === (0 || 1) ) ){
-      console.log('FizzBuzz');
-    }
-    else{
-      console.log( iterate );
-    }
-    iterate ++;
+  if ((num % 3 === 0) && (num % 5 === 0)) {
+
+    console.log("FizzBuzz")
+
+  }
+  if (num % 3 === 0) {
+    console.log("Fizz")
+
+
+  } else if (num % 5 === 0) {
+    console.log("Buzz")
+
+
+  } else {
+    console.log(num);
+
+  }
+  num++;
 }
+
+
+
+// }
 
 //  iterate / 3 % 2 === 0 && iterate / 5 % 2 === 0
 
